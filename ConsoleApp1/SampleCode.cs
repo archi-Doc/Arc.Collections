c13b8dcadf0d468a9b368fce4ab8e35f7c33b4af
archi-Doc <archidoc422@gmail.com>
2021-10-04 17:47:16 +0900

    .

---
+++ ConsoleApp1/SampleCode.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1;
+
+internal class SampleCode
+{
+}
