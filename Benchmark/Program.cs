8e112f077e47187241efffc81eefc95518ccc1c8
archi-Doc <archidoc422@gmail.com>
2025-09-27 21:12:11 +0900

    .

--- Benchmark/Program.cs
+++ Benchmark/Program.cs
@@ -17,7 +17,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        DebugRun<CountDecimalCharsBenchmark>();
+        DebugRun<ObjectPoolBenchmark>();
 
         // var summary = BenchmarkRunner.Run<TestBenchmark>();
         var switcher = new BenchmarkSwitcher(new[]
