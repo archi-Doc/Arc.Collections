86ac96a1eb8670d13aaf63c77288fd07e3a89df1
archi-Doc <archidoc422@gmail.com>
2026-08-19 23:25:14 +0900

    .

--- Benchmark/Benchmark/CircularQueueBenchmark.cs
+++ Benchmark/Benchmark/CircularQueueBenchmark.cs
@@ -1,51 +1,112 @@
 ﻿using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
 using Arc.Collections;
+using Benchmark;
 using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Running;
 
-[MemoryDiagnoser]
+/// <summary>
+/// Multi-threaded producer/consumer benchmark.
+/// </summary>
+[Config(typeof(BenchmarkConfig))]
 public class CircularQueueBenchmark
 {
     private const int Capacity = 1024;
-    private const int Operations = 1000;
+    private const int ItemsPerProducer = 10_000;
 
-    private CircularQueue<int> circularQueue = null!;
-    private ConcurrentQueue<int> concurrentQueue = null!;
+    [Params(1, 2, 4)]
+    public int ThreadPairs { get; set; } // number of producer threads == number of consumer threads
 
-    [GlobalSetup]
-    public void Setup()
+    private CircularQueue<int> circularQueue = default!;
+    private ConcurrentQueue<int> concurrentQueue = default!;
+
+    [IterationSetup]
+    public void IterationSetup()
     {
         this.circularQueue = new CircularQueue<int>(Capacity);
         this.concurrentQueue = new ConcurrentQueue<int>();
     }
 
     [Benchmark(Baseline = true)]
-    public int CircularQueue()
+    public void CircularQueue_ProducerConsumer()
     {
-        var sum = 0;
+        var queue = this.circularQueue;
+        var total = this.ThreadPairs * ItemsPerProducer;
+        var consumed = 0;
+        var tasks = new Task[this.ThreadPairs * 2];
 
-        for (var i = 0; i < Operations; i++)
+        for (var i = 0; i < this.ThreadPairs; i++)
         {
-            this.circularQueue.TryEnqueue(i);
-            this.circularQueue.TryDequeue(out var value);
-            sum += value;
+            tasks[i] = Task.Run(() =>
+            {
+                for (var j = 0; j < ItemsPerProducer; j++)
+                {
+                    while (!queue.TryEnqueue(j))
+                    {
+                        Thread.SpinWait(1); // Queue is full: spin until a slot is available.
+                    }
+                }
+            });
+
+            tasks[this.ThreadPairs + i] = Task.Run(() =>
+            {
+                while (Volatile.Read(ref consumed) < total)
+                {
+                    if (queue.TryDequeue(out _))
+                    {
+                        Interlocked.Increment(ref consumed);
+                    }
+                    else
+                    {
+                        Thread.SpinWait(1);
+                    }
+                }
+            });
         }
 
-        return sum;
+        Task.WaitAll(tasks);
     }
 
     [Benchmark]
-    public int ConcurrentQueue()
+    public void ConcurrentQueue_ProducerConsumer()
     {
-        var sum = 0;
+        var queue = this.concurrentQueue;
+        var total = this.ThreadPairs * ItemsPerProducer;
+        var consumed = 0;
+        var tasks = new Task[this.ThreadPairs * 2];
 
-        for (var i = 0; i < Operations; i++)
+        for (var i = 0; i < this.ThreadPairs; i++)
         {
-            this.concurrentQueue.Enqueue(i);
-            this.concurrentQueue.TryDequeue(out var value);
-            sum += value;
+            tasks[i] = Task.Run(() =>
+            {
+                for (var j = 0; j < ItemsPerProducer; j++)
+                {
+                    // Emulate the bounded limit of CircularQueue.
+                    while (queue.Count >= Capacity)
+                    {
+                        Thread.SpinWait(1);
+                    }
+
+                    queue.Enqueue(j);
+                }
+            });
+
+            tasks[this.ThreadPairs + i] = Task.Run(() =>
+            {
+                while (Volatile.Read(ref consumed) < total)
+                {
+                    if (queue.TryDequeue(out _))
+                    {
+                        Interlocked.Increment(ref consumed);
+                    }
+                    else
+                    {
+                        Thread.SpinWait(1);
+                    }
+                }
+            });
         }
 
-        return sum;
+        Task.WaitAll(tasks);
     }
 }
