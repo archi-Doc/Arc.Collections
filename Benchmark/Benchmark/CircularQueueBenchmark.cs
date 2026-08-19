b2af42c35b5f5857c27fa0035734b992e1e65ccd
archi-Doc <archidoc422@gmail.com>
2026-08-19 22:54:47 +0900

    .

---
+++ Benchmark/Benchmark/CircularQueueBenchmark.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Concurrent;
+using Arc.Collections;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+[MemoryDiagnoser]
+public class CircularQueueBenchmark
+{
+    private const int Capacity = 1024;
+    private const int Operations = 1000;
+
+    private CircularQueue<int> circularQueue = null!;
+    private ConcurrentQueue<int> concurrentQueue = null!;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        this.circularQueue = new CircularQueue<int>(Capacity);
+        this.concurrentQueue = new ConcurrentQueue<int>();
+    }
+
+    [Benchmark(Baseline = true)]
+    public int CircularQueue()
+    {
+        var sum = 0;
+
+        for (var i = 0; i < Operations; i++)
+        {
+            this.circularQueue.TryEnqueue(i);
+            this.circularQueue.TryDequeue(out var value);
+            sum += value;
+        }
+
+        return sum;
+    }
+
+    [Benchmark]
+    public int ConcurrentQueue()
+    {
+        var sum = 0;
+
+        for (var i = 0; i < Operations; i++)
+        {
+            this.concurrentQueue.Enqueue(i);
+            this.concurrentQueue.TryDequeue(out var value);
+            sum += value;
+        }
+
+        return sum;
+    }
+}
--- Benchmark/Program.cs
+++ Benchmark/Program.cs
@@ -22,6 +22,7 @@ public class Program
         // var summary = BenchmarkRunner.Run<TestBenchmark>();
         var switcher = new BenchmarkSwitcher(new[]
         {
+            typeof(CircularQueueBenchmark),
             typeof(SequenceBuilderAddBenchmark),
             typeof(StringBuilderBenchmark),
             typeof(HashCombinerBenchmark),
