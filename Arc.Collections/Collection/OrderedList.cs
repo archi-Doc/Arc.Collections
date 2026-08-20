8dd954744fec842ffa2aacf80173831ef208488c
archi-Doc <archidoc422@gmail.com>
2026-08-20 09:30:55 +0900

    .

--- Arc.Collections/Collection/OrderedList.cs
+++ Arc.Collections/Collection/OrderedList.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
 
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Arc.Collections.HotMethod;
@@ -9,7 +8,8 @@ using Arc.Collections.HotMethod;
 namespace Arc.Collections;
 
 /// <summary>
-/// Represents a list of objects that can be accessed by index and maintained in sorted order (ascending by default).
+/// Represents a list of elements maintained in sorted order.
+/// Duplicate elements are allowed and preserve their insertion order.
 /// </summary>
 /// <typeparam name="T">The type of elements in the list.</typeparam>
 public class OrderedList<T> : UnorderedList<T>
@@ -18,9 +18,8 @@ public class OrderedList<T> : UnorderedList<T>
     /// Initializes a new instance of the <see cref="OrderedList{T}"/> class.
     /// </summary>
     public OrderedList()
+        : this(0, null)
     {
-        this.Comparer = Comparer<T>.Default;
-        this.HotMethod = HotMethodResolver.Get<T>(this.Comparer);
     }
 
     /// <summary>
@@ -28,17 +27,26 @@ public class OrderedList<T> : UnorderedList<T>
     /// </summary>
     /// <param name="capacity">The number of elements that the new list can initially store.</param>
     public OrderedList(int capacity)
-        : base(capacity)
+        : this(capacity, null)
     {
-        this.Comparer = Comparer<T>.Default;
-        this.HotMethod = HotMethodResolver.Get<T>(this.Comparer);
     }
 
     /// <summary>
     /// Initializes a new instance of the <see cref="OrderedList{T}"/> class.
     /// </summary>
-    /// <param name="comparer">The default comparer to use for comparing objects.</param>
-    public OrderedList(IComparer<T> comparer)
+    /// <param name="comparer">The comparer to use for comparing elements.</param>
+    public OrderedList(IComparer<T>? comparer)
+        : this(0, comparer)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="OrderedList{T}"/> class.
+    /// </summary>
+    /// <param name="capacity">The number of elements that the new list can initially store.</param>
+    /// <param name="comparer">The comparer to use for comparing elements.</param>
+    public OrderedList(int capacity, IComparer<T>? comparer)
+        : base(capacity)
     {
         this.Comparer = comparer ?? Comparer<T>.Default;
         this.HotMethod = HotMethodResolver.Get<T>(this.Comparer);
@@ -49,7 +57,7 @@ public class OrderedList<T> : UnorderedList<T>
     /// </summary>
     /// <param name="collection">The collection whose elements are copied to the new list.</param>
     public OrderedList(IEnumerable<T> collection)
-        : this(collection, Comparer<T>.Default)
+        : this(collection, null)
     {
     }
 
@@ -57,105 +65,99 @@ public class OrderedList<T> : UnorderedList<T>
     /// Initializes a new instance of the <see cref="OrderedList{T}"/> class.
     /// </summary>
     /// <param name="collection">The collection whose elements are copied to the new list.</param>
-    /// <param name="comparer">The default comparer to use for comparing objects.</param>
-    public OrderedList(IEnumerable<T> collection, IComparer<T> comparer)
+    /// <param name="comparer">The comparer to use for comparing elements.</param>
+    public OrderedList(IEnumerable<T> collection, IComparer<T>? comparer)
     {
+        ArgumentNullException.ThrowIfNull(collection);
+
         this.Comparer = comparer ?? Comparer<T>.Default;
         this.HotMethod = HotMethodResolver.Get<T>(this.Comparer);
-        if (collection == null)
-        {
-            throw new ArgumentNullException(nameof(collection));
-        }
 
         var array = collection.ToArray();
-        Array.Sort(array, this.Comparer);
-        this.size = array.Length;
+        if (array.Length > 1)
+        {
+            Array.Sort(array, this.Comparer);
+        }
+
         this.items = array;
+        this.size = array.Length;
     }
 
-    public IComparer<T> Comparer { get; private set; }
+    public IComparer<T> Comparer { get; }
 
-    public IHotMethod<T>? HotMethod { get; private set; }
+    public IHotMethod<T>? HotMethod { get; }
 
     /// <summary>
-    /// Adds an object to the list in order.
-    /// <br/>O(n) operation.
+    /// Adds an element while maintaining sorted order.
+    /// Elements equal to existing elements are inserted after them.
     /// </summary>
-    /// <param name="value">The value to be added to the list.</param>
+    /// <param name="value">The value to add.</param>
     public new void Add(T value)
     {
-        var pos = this.BinarySearch(value);
-        if (pos < 0)
-        {
-            this.Insert(~pos, value);
-        }
-        else
-        {// Adds to the end of the same values.
-            pos++;
-            while (pos < this.size && this.Comparer.Compare(this.items[pos], value) == 0)
-            {
-                pos++;
-            }
-
-            this.Insert(pos, value);
-        }
+        this.Insert(this.UpperBoundExclusive(value), value);
     }
 
     /// <summary>
-    /// Searches a list for the specific value.
+    /// Searches for the specified value.
     /// </summary>
     /// <param name="value">The value to search for.</param>
-    /// <returns>The index of the specified value in list. If the value is not found, the negative number returned is the bitwise complement of the index of the first element that is larger than value.</returns>
-    public int BinarySearch(T value) // => Array.BinarySearch(this.items, 0, this.size, value, this.Comparer);
+    /// <returns>
+    /// An index of a matching element, or the bitwise complement of its insertion index
+    /// if no matching element exists.
+    /// </returns>
+    public int BinarySearch(T value)
     {
-        if (this.HotMethod != null)
+        var hotMethod = this.HotMethod;
+        if (hotMethod is not null)
         {
-            return this.HotMethod.BinarySearch(this.items, 0, this.size, value);
+            return hotMethod.BinarySearch(this.items, 0, this.size, value);
         }
 
+        var comparer = this.Comparer;
         var min = 0;
         var max = this.size - 1;
 
-        if (this.Comparer == Comparer<T>.Default && value is IComparable<T> ic)
-        {// IComparable<T>
+        // Avoid boxing value types through IComparable<T>.
+        if (!typeof(T).IsValueType &&
+            ReferenceEquals(comparer, Comparer<T>.Default) &&
+            value is IComparable<T> comparable)
+        {
             while (min <= max)
             {
-                var mid = min + ((max - min) / 2);
-                var cmp = ic.CompareTo(this.items[mid]); // -1: 1st < 2nd, 0: equals, 1: 1st > 2nd
-                if (cmp < 0)
+                var mid = min + ((max - min) >> 1);
+                var comparison = comparable.CompareTo(this.items[mid]);
+
+                if (comparison < 0)
                 {
                     max = mid - 1;
-                    continue;
                 }
-                else if (cmp > 0)
+                else if (comparison > 0)
                 {
                     min = mid + 1;
-                    continue;
                 }
                 else
-                {// Found
+                {
                     return mid;
                 }
             }
         }
         else
-        {// IComparer<T>
+        {
             while (min <= max)
             {
-                var mid = min + ((max - min) / 2);
-                var cmp = this.Comparer.Compare(value, this.items[mid]); // -1: 1st < 2nd, 0: equals, 1: 1st > 2nd
-                if (cmp < 0)
+                var mid = min + ((max - min) >> 1);
+                var comparison = comparer.Compare(value, this.items[mid]);
+
+                if (comparison < 0)
                 {
                     max = mid - 1;
-                    continue;
                 }
-                else if (cmp > 0)
+                else if (comparison > 0)
                 {
                     min = mid + 1;
-                    continue;
                 }
                 else
-                {// Found
+                {
                     return mid;
                 }
             }
@@ -165,127 +167,177 @@ public class OrderedList<T> : UnorderedList<T>
     }
 
     /// <summary>
-    /// Get the index of the first element equal to or greater than the specified value (-1: all elements are less than the specified value).
+    /// Gets the index of the first element equal to or greater than the specified value.
     /// </summary>
     /// <param name="value">The value to search for.</param>
-    /// <returns>The index of the first element equal to or greater than the specified value (-1: all elements are less than the specified value).</returns>
+    /// <returns>The index, or -1 if all elements are less than the specified value.</returns>
     public int GetLowerBound(T value)
     {
-        var index = this.BinarySearch(value);
-        if (index >= 0)
-        {
-            if (this.Comparer == Comparer<T>.Default && value is IComparable<T> ic)
-            {// IComparable<T>
-                while (index > 0 &&
-                ic.CompareTo(this.items[index - 1]) == 0)
-                {
-                    index--;
-                }
-            }
-            else
-            {
-                while (index > 0 &&
-                this.Comparer.Compare(value, this.items[index - 1]) == 0)
-                {
-                    index--;
-                }
-            }
-
-            return index;
-        }
-        else
-        {
-            return ~index < this.items.Length ? ~index : -1;
-        }
+        var index = this.LowerBound(value);
+        return index < this.size ? index : -1;
     }
 
     /// <summary>
-    /// Get the index of the last element equal to or lower than the specified value (-1: all elements are greater than the specified value).
+    /// Gets the index of the last element equal to or less than the specified value.
     /// </summary>
     /// <param name="value">The value to search for.</param>
-    /// <returns>The index of the last element equal to or lower than the specified value (-1: all elements are greater than the specified value).</returns>
+    /// <returns>The index, or -1 if all elements are greater than the specified value.</returns>
     public int GetUpperBound(T value)
     {
-        var index = this.BinarySearch(value);
-        if (index >= 0)
-        {
-            if (this.Comparer == Comparer<T>.Default && value is IComparable<T> ic)
-            {// IComparable<T>
-                while (index < this.items.Length - 1 &&
-                ic.CompareTo(this.items[index + 1]) == 0)
-                {
-                    index++;
-                }
-            }
-            else
-            {
-                while (index < this.items.Length - 1 &&
-                this.Comparer.Compare(value, this.items[index + 1]) == 0)
-                {
-                    index++;
-                }
-            }
-
-            return index;
-        }
-        else
-        {
-            return ~index - 1;
-        }
+        return this.UpperBoundExclusive(value) - 1;
     }
 
-    // public int ArrayBinarySearch(T value) => Array.BinarySearch(this.items, 0, this.size, value, this.Comparer);
+    /// <summary>
+    /// Determines whether the specified value is in the list.
+    /// </summary>
+    /// <param name="value">The value to locate.</param>
+    /// <returns><see langword="true"/> if the value is found.</returns>
+    public new bool Contains(T value)
+    {
+        var index = this.LowerBound(value);
+        return index < this.size && this.Comparer.Compare(this.items[index], value) == 0;
+    }
 
     /// <summary>
-    /// Determines whether an element is in the list.
-    /// <br/>O(log n) operation.
+    /// Removes the first occurrence of the specified value.
     /// </summary>
-    /// <param name="value">The value to locate in the list.</param>
-    /// <returns>true if value is found in the list.</returns>
-    public new bool Contains(T value) => this.BinarySearch(value) >= 0;
-
-    /// <summary>
-    /// Removes the first occurrence of a specific object from the <see cref="UnorderedList{T}"/>.
-    /// <br/>O(n) operation.
-    /// </summary>
-    /// <param name="value">The object to remove from the <see cref="UnorderedList{T}"/>. </param>
-    /// <returns>true if item is successfully removed.</returns>
+    /// <param name="value">The value to remove.</param>
+    /// <returns><see langword="true"/> if the value was removed.</returns>
     public new bool Remove(T value)
     {
-        var index = this.BinarySearch(value);
-        if (index >= 0)
+        var index = this.IndexOf(value);
+        if (index < 0)
         {
-            this.RemoveAt(index);
-            return true;
+            return false;
         }
 
-        return false;
+        this.RemoveAt(index);
+        return true;
     }
 
     public new T this[int index]
     {
         get
         {
-            // Following trick can reduce the range check by one
             if ((uint)index >= (uint)this.size)
             {
-                throw new ArgumentOutOfRangeException();
+                throw new ArgumentOutOfRangeException(nameof(index));
             }
 
             return this.items[index];
         }
 
-        set
-        {
-            throw new InvalidOperationException();
-        }
+        set => throw new InvalidOperationException("Elements cannot be replaced directly in an ordered list.");
     }
 
     /// <summary>
-    /// Returns the zero-based index of the first occurrence of a value in the list.
-    /// <br/>O(log n) operation.
+    /// Returns the index of the first occurrence of the specified value.
     /// </summary>
-    /// <param name="value">The value to locate in the list.</param>
-    /// <returns>The zero-based index of the first occurrence of item.</returns>
-    public new int IndexOf(T value) => this.BinarySearch(value);
+    /// <param name="value">The value to locate.</param>
+    /// <returns>The first matching index, or -1 if the value is not found.</returns>
+    public new int IndexOf(T value)
+    {
+        var index = this.LowerBound(value);
+
+        if (index < this.size && this.Comparer.Compare(this.items[index], value) == 0)
+        {
+            return index;
+        }
+
+        return -1;
+    }
+
+    /// <summary>
+    /// Returns the index of the first element not less than the specified value.
+    /// </summary>
+    private int LowerBound(T value)
+    {
+        var min = 0;
+        var max = this.size;
+        var comparer = this.Comparer;
+
+        if (!typeof(T).IsValueType &&
+            ReferenceEquals(comparer, Comparer<T>.Default) &&
+            value is IComparable<T> comparable)
+        {
+            while (min < max)
+            {
+                var mid = min + ((max - min) >> 1);
+
+                if (comparable.CompareTo(this.items[mid]) > 0)
+                {
+                    min = mid + 1;
+                }
+                else
+                {
+                    max = mid;
+                }
+            }
+        }
+        else
+        {
+            while (min < max)
+            {
+                var mid = min + ((max - min) >> 1);
+
+                if (comparer.Compare(this.items[mid], value) < 0)
+                {
+                    min = mid + 1;
+                }
+                else
+                {
+                    max = mid;
+                }
+            }
+        }
+
+        return min;
+    }
+
+    /// <summary>
+    /// Returns the index of the first element greater than the specified value.
+    /// </summary>
+    private int UpperBoundExclusive(T value)
+    {
+        var min = 0;
+        var max = this.size;
+        var comparer = this.Comparer;
+
+        if (!typeof(T).IsValueType &&
+            ReferenceEquals(comparer, Comparer<T>.Default) &&
+            value is IComparable<T> comparable)
+        {
+            while (min < max)
+            {
+                var mid = min + ((max - min) >> 1);
+
+                if (comparable.CompareTo(this.items[mid]) >= 0)
+                {
+                    min = mid + 1;
+                }
+                else
+                {
+                    max = mid;
+                }
+            }
+        }
+        else
+        {
+            while (min < max)
+            {
+                var mid = min + ((max - min) >> 1);
+
+                if (comparer.Compare(this.items[mid], value) <= 0)
+                {
+                    min = mid + 1;
+                }
+                else
+                {
+                    max = mid;
+                }
+            }
+        }
+
+        return min;
+    }
 }
---
+++ xUnitTest/OrderedListTest2.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using Arc.Collections;
+using Xunit;
+
+namespace XunitTest;
+
+public class OrderedListTest2
+{
+    [Fact]
+    public void Add_SortsElements()
+    {
+        var list = new OrderedList<int>();
+
+        list.Add(3);
+        list.Add(1);
+        list.Add(2);
+
+        Assert.Equal(3, list.Count);
+        Assert.Equal(1, list[0]);
+        Assert.Equal(2, list[1]);
+        Assert.Equal(3, list[2]);
+    }
+
+    [Fact]
+    public void Add_AllowsDuplicates()
+    {
+        var list = new OrderedList<int>();
+
+        list.Add(2);
+        list.Add(1);
+        list.Add(2);
+        list.Add(2);
+        list.Add(3);
+
+        Assert.Equal(5, list.Count);
+        Assert.Equal(new[] { 1, 2, 2, 2, 3 }, list);
+    }
+
+    [Fact]
+    public void Constructor_SortsCollection()
+    {
+        var list = new OrderedList<int>(new[] { 5, 1, 4, 2, 3 });
+
+        Assert.Equal(new[] { 1, 2, 3, 4, 5 }, list);
+    }
+
+    [Theory]
+    [InlineData(1, 0)]
+    [InlineData(2, 1)]
+    [InlineData(3, 4)]
+    [InlineData(4, 5)]
+    public void IndexOf_ReturnsFirstOccurrence(int value, int expected)
+    {
+        var list = new OrderedList<int>(new[] { 1, 2, 2, 2, 3, 4 });
+
+        Assert.Equal(expected, list.IndexOf(value));
+    }
+
+    [Fact]
+    public void IndexOf_ReturnsMinusOne_WhenNotFound()
+    {
+        var list = new OrderedList<int>(new[] { 1, 2, 3 });
+
+        Assert.Equal(-1, list.IndexOf(4));
+    }
+
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(1, 0)]
+    [InlineData(2, 1)]
+    [InlineData(3, 4)]
+    [InlineData(4, 5)]
+    [InlineData(5, -1)]
+    public void GetLowerBound_ReturnsExpectedIndex(int value, int expected)
+    {
+        var list = new OrderedList<int>(new[] { 1, 2, 2, 2, 3, 4 });
+
+        Assert.Equal(expected, list.GetLowerBound(value));
+    }
+
+    [Theory]
+    [InlineData(0, -1)]
+    [InlineData(1, 0)]
+    [InlineData(2, 3)]
+    [InlineData(3, 4)]
+    [InlineData(4, 5)]
+    [InlineData(5, 5)]
+    public void GetUpperBound_ReturnsExpectedIndex(int value, int expected)
+    {
+        var list = new OrderedList<int>(new[] { 1, 2, 2, 2, 3, 4 });
+
+        Assert.Equal(expected, list.GetUpperBound(value));
+    }
+
+    [Fact]
+    public void Contains_ReturnsExpectedResult()
+    {
+        var list = new OrderedList<int>(new[] { 1, 2, 2, 3 });
+
+        Assert.True(list.Contains(2));
+        Assert.False(list.Contains(4));
+    }
+
+    [Fact]
+    public void Remove_RemovesFirstOccurrence()
+    {
+        var list = new OrderedList<int>(new[] { 1, 2, 2, 2, 3 });
+
+        Assert.True(list.Remove(2));
+
+        Assert.Equal(new[] { 1, 2, 2, 3 }, list);
+    }
+
+    [Fact]
+    public void Remove_ReturnsFalse_WhenNotFound()
+    {
+        var list = new OrderedList<int>(new[] { 1, 2, 3 });
+
+        Assert.False(list.Remove(4));
+        Assert.Equal(new[] { 1, 2, 3 }, list);
+    }
+
+    [Fact]
+    public void BinarySearch_FindsExistingValue()
+    {
+        var list = new OrderedList<int>(new[] { 1, 2, 2, 3, 4 });
+
+        var index = list.BinarySearch(2);
+
+        Assert.InRange(index, 1, 2);
+        Assert.Equal(2, list[index]);
+    }
+
+    [Fact]
+    public void BinarySearch_ReturnsInsertionIndex_WhenNotFound()
+    {
+        var list = new OrderedList<int>(new[] { 1, 3, 5 });
+
+        var index = list.BinarySearch(4);
+
+        Assert.Equal(2, ~index);
+    }
+
+    [Fact]
+    public void CustomComparer_SortsUsingComparer()
+    {
+        var list = new OrderedList<int>(
+            new[] { 1, 3, 2, 5, 4 },
+            Comparer<int>.Create(static (x, y) => y.CompareTo(x)));
+
+        Assert.Equal(new[] { 5, 4, 3, 2, 1 }, list);
+    }
+
+    [Fact]
+    public void IndexerSetter_Throws()
+    {
+        var list = new OrderedList<int>(new[] { 1, 2, 3 });
+
+        Assert.Throws<InvalidOperationException>(() => list[1] = 10);
+    }
+
+    [Fact]
+    public void EmptyList_BoundsAreMinusOne()
+    {
+        var list = new OrderedList<int>();
+
+        Assert.Equal(-1, list.GetLowerBound(1));
+        Assert.Equal(-1, list.GetUpperBound(1));
+        Assert.Equal(-1, list.IndexOf(1));
+        Assert.False(list.Contains(1));
+    }
+}
