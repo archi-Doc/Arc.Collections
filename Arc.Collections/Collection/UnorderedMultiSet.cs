5079e4ce165c2953ee8a44fc4d3d4f6dc0e05c8c
archi-Doc <archidoc422@gmail.com>
2026-08-20 10:28:35 +0900

    .

--- Arc.Collections/Collection/UnorderedMultiSet.cs
+++ Arc.Collections/Collection/UnorderedMultiSet.cs
@@ -9,12 +9,13 @@ using System.Collections.Generic;
 namespace Arc.Collections;
 
 /// <summary>
-/// Represents a collection of objects.
-/// <br/><see cref="UnorderedMultiSet{T}"/> uses a hash table structure to store objects.
+/// Represents an unordered collection that allows duplicate elements.
 /// </summary>
-/// <typeparam name="T">The type of elements in the set.</typeparam>
-public class UnorderedMultiSet<T> : ICollection<T>, IReadOnlyCollection<T>, ICollection
+/// <typeparam name="T">The type of elements in the collection.</typeparam>
+public sealed class UnorderedMultiSet<T> : ICollection<T>, IReadOnlyCollection<T>, ICollection
 {
+    private readonly UnorderedMultiMap<T, int> map;
+
     /// <summary>
     /// Initializes a new instance of the <see cref="UnorderedMultiSet{T}"/> class.
     /// </summary>
@@ -26,94 +27,89 @@ public class UnorderedMultiSet<T> : ICollection<T>, IReadOnlyCollection<T>, ICol
     /// <summary>
     /// Initializes a new instance of the <see cref="UnorderedMultiSet{T}"/> class.
     /// </summary>
-    /// <param name="comparer">The default comparer to use for comparing objects.</param>
+    /// <param name="comparer">The equality comparer to use for the elements.</param>
     public UnorderedMultiSet(IEqualityComparer<T> comparer)
     {
         this.map = new(comparer);
     }
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="UnorderedMultiSet{T}"/> class.
+    /// Initializes a new instance of the <see cref="UnorderedMultiSet{T}"/> class containing the specified elements.
     /// </summary>
-    /// <param name="collection">The enumerable collection to be copied.</param>
+    /// <param name="collection">The elements to add to the collection.</param>
     public UnorderedMultiSet(IEnumerable<T> collection)
         : this(collection, EqualityComparer<T>.Default)
     {
     }
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="UnorderedMultiSet{T}"/> class.
+    /// Initializes a new instance of the <see cref="UnorderedMultiSet{T}"/> class containing the specified elements.
     /// </summary>
-    /// <param name="collection">The enumerable collection to be copied.</param>
-    /// <param name="comparer">The default comparer to use for comparing objects.</param>
+    /// <param name="collection">The elements to add to the collection.</param>
+    /// <param name="comparer">The equality comparer to use for the elements.</param>
     public UnorderedMultiSet(IEnumerable<T> collection, IEqualityComparer<T> comparer)
     {
-        this.map = new(comparer);
+        ArgumentNullException.ThrowIfNull(collection);
 
-        foreach (var x in collection)
+        this.map = new(comparer);
+        foreach (var item in collection)
         {
-            this.Add(x);
+            this.map.Add(item, 0);
         }
     }
-
-    private UnorderedMultiMap<T, int> map;
 
     #region Main
 
     /// <summary>
-    /// Gets the number of nodes actually contained in the <see cref="UnorderedMultiSet{T}"/>.
+    /// Gets the number of elements contained in the collection, including duplicates.
     /// </summary>
     public int Count => this.map.Count;
 
     /// <summary>
-    /// Adds an element to a collection. If the element is already in the set, this method returns the stored element without creating a new node, and sets NewlyAdded to false.
-    /// <br/>O(1) operation.
+    /// Adds the specified value to the collection.
     /// </summary>
-    /// <param name="value">The value of the element to add.</param>
-    /// <returns>NodeIndex: the added node index.<br/>
-    /// NewlyAdded: true if the node is created.</returns>
+    /// <param name="value">The value to add.</param>
+    /// <returns>The node index and whether a new key entry was created.</returns>
     public (int NodeIndex, bool NewlyAdded) Add(T value)
-    {
-        var result = this.map.Add(value, 0);
-        return result;
-    }
+        => this.map.Add(value, 0);
 
     /// <summary>
-    /// Determines whether a collection contains a specific value.
-    /// <br/>O(1) operation.
+    /// Determines whether the collection contains the specified value.
     /// </summary>
-    /// <param name="value">The value to locate in the collection.</param>
-    /// <returns>true if the collection contains an element with the specified value; otherwise, false.</returns>
-    public bool Contains(T value) => this.map.ContainsKey(value);
+    /// <param name="value">The value to locate.</param>
+    /// <returns><see langword="true"/> if the value is contained in the collection; otherwise, <see langword="false"/>.</returns>
+    public bool Contains(T value)
+        => this.map.ContainsKey(value);
 
     /// <summary>
-    /// Updates the node's value with the specified value. Removes the node and inserts it in the correct position if necessary.
-    /// <br/>O(1) operation.
+    /// Replaces the value of the specified node.
     /// </summary>
-    /// <param name="nodeIndex">The <see cref="UnorderedMap{TKey, TValue}.Node"/> to set the value.</param>
-    /// <param name="value">The value to set.</param>
-    /// <returns>true if the node is successfully updated.</returns>
-    public bool SetNodeValue(int nodeIndex, T? value) => this.map.SetNodeKey(nodeIndex, value);
+    /// <param name="nodeIndex">The index of the node to update.</param>
+    /// <param name="value">The new value.</param>
+    /// <returns><see langword="true"/> if the node was successfully updated; otherwise, <see langword="false"/>.</returns>
+    public bool SetNodeValue(int nodeIndex, T value)
+        => this.map.SetNodeKey(nodeIndex, value);
 
     /// <summary>
-    /// Removes a specified value from the collection."/>.
-    /// <br/>O(1) operation.
+    /// Removes one occurrence of the specified value from the collection.
     /// </summary>
-    /// <param name="value">The element to remove.</param>
-    /// <returns>true if the element is found and successfully removed.</returns>
-    public bool Remove(T value) => this.map.Remove(value);
+    /// <param name="value">The value to remove.</param>
+    /// <returns><see langword="true"/> if an occurrence was found and removed; otherwise, <see langword="false"/>.</returns>
+    public bool Remove(T value)
+        => this.map.Remove(value);
 
     /// <summary>
-    /// Removes a specified node from the collection.
-    /// <br/>O(1) operation.
+    /// Removes the specified node from the collection.
     /// </summary>
     /// <param name="nodeIndex">The index of the node to remove.</param>
-    public void RemoveNode(int nodeIndex) => this.map.RemoveNode(nodeIndex);
+    public void RemoveNode(int nodeIndex)
+        => this.map.RemoveNode(nodeIndex);
 
     /// <summary>
-    /// Removes all elements from a collection.
+    /// Removes all elements from the collection.
     /// </summary>
-    public void Clear() => this.map.Clear();
+    public void Clear()
+        => this.map.Clear();
 
     #endregion
 
@@ -125,17 +121,23 @@ public class UnorderedMultiSet<T> : ICollection<T>, IReadOnlyCollection<T>, ICol
 
     object ICollection.SyncRoot => this;
 
-    void ICollection<T>.Add(T item) => this.map.Add(item, 0);
+    void ICollection<T>.Add(T item)
+        => this.map.Add(item, 0);
 
-    void ICollection<T>.CopyTo(T[] array, int arrayIndex) => this.map.Keys.CopyTo(array, arrayIndex);
+    void ICollection<T>.CopyTo(T[] array, int arrayIndex)
+        => this.map.Keys.CopyTo(array, arrayIndex);
 
-    void ICollection.CopyTo(Array array, int index) => ((ICollection)this.map.Keys).CopyTo(array, index);
+    void ICollection.CopyTo(Array array, int index)
+        => ((ICollection)this.map.Keys).CopyTo(array, index);
 
-    public UnorderedMultiMap<T, int>.KeyCollection.Enumerator GetEnumerator() => this.map.Keys.GetEnumerator();
+    public UnorderedMultiMap<T, int>.KeyCollection.Enumerator GetEnumerator()
+        => this.map.Keys.GetEnumerator();
 
-    IEnumerator<T> IEnumerable<T>.GetEnumerator() => this.map.Keys.GetEnumerator();
+    IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        => this.map.Keys.GetEnumerator();
 
-    IEnumerator IEnumerable.GetEnumerator() => this.map.Keys.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator()
+        => this.map.Keys.GetEnumerator();
 
     #endregion
 }
--- Arc.Collections/Collection/UnorderedSet.cs
+++ Arc.Collections/Collection/UnorderedSet.cs
@@ -3,6 +3,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 #pragma warning disable SA1124 // Do not use regions
 
